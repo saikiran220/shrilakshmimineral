c78f663e30db4816efb99066f9e138130f2cd478
bolakondasaikiran <127081555+saikiran220@users.noreply.github.com>
2025-07-29 23:25:02 +0530

    x
    w

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+    cssCodeSplit: false,
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
